60e7b5abb281df5ca7539613e9d6cbb29d0c765d
Sarun <sarunmodi123@gmail.com>
2026-08-30 08:00:15 +0100

    Got streamlit app working, must go to terminal and write streamlit run app.py, added dropdown with each currency automated by opening model_paths.json

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Hello")
+import streamlit as st
+import pandas as pd
+import json
+st.set_page_config(page_title = "Forex Currency Prediction")
+st.title("Forex Currency Prediction")
+with open("models/model_paths.json", "r") as file:
+    model_paths = json.load(file)
+currencies = [key for key in model_paths.keys()]
+
+# create dropdown
+chosen_currency = st.selectbox("Choose Currency", currencies)
+
